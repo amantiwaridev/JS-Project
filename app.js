53a8721c74a645cfc6cac55837027495c35cab78
Aman Tiwari <a.tiwari@student.utwente.nl>
2023-07-02 21:18:46 +0530

    feat-translation completed

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input){
+
+    return serverURL + "?" + "text=" + input
+
+}
+
 
 
 function clickHandler(){   
-        outputDiv.innerText = "bananananbanana " + txtInput.value;
+        var inputText = txtInput.value;
+
+        //calling server for translation
+        fetch(getTranslationURL(inputText))
+            .then(promise => promise.json())
+            .then(json => {
+                // var translatedText = json.contents.translated;
+                // outputDiv.innerText = translatedText;
+                console.log(json)
+            })
     }
 
-    btnTranslate.addEventListener("click", clickHandler);
+btnTranslate.addEventListener("click", clickHandler);
